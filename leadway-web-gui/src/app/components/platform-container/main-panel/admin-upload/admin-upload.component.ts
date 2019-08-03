a75c042ad4b67e7cddd337c7a378b7853220a92d
Henry <whchen1@uci.edu>
2019-08-03 12:05:56 -0700

    update admin-upload page style

--- leadway-web-gui/src/app/components/platform-container/main-panel/admin-upload/admin-upload.component.ts
+++ leadway-web-gui/src/app/components/platform-container/main-panel/admin-upload/admin-upload.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { MatSelectionList, MatSelectionListChange, MatListOption } from '@angular/material';
 
 @Component({
   selector: 'leadway-admin-upload',
@@ -9,13 +8,11 @@ import { MatSelectionList, MatSelectionListChange, MatListOption } from '@angula
   styleUrls: ['./admin-upload.component.scss']
 })
 export class AdminUploadComponent implements OnInit {
-  items: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+
   public taskFormGroup: FormGroup;
-  private selected :MatListOption =null;
-  @ViewChild(MatSelectionList,{static: true}) selectionList: MatSelectionList;
-  
-  constructor(private router: Router, private formBuilder: FormBuilder) {
-    
+
+  constructor(private formBuilder: FormBuilder) {
+
     this.taskFormGroup = formBuilder.group({
       taskFunding: new FormControl('', Validators.compose([
         Validators.pattern(/^[0-9]+$/),
@@ -25,35 +22,20 @@ export class AdminUploadComponent implements OnInit {
         Validators.pattern(/^[0-9]+$/),
         Validators.required
       ])),
-      startDate:new FormControl('',Validators.compose([
+      startDate: new FormControl('', Validators.compose([
         Validators.required
       ])),
-      endDate:new FormControl('',Validators.compose([
+      endDate: new FormControl('', Validators.compose([
         Validators.required
       ])),
-      taskDescription:new FormControl(),
-      taskType:new FormControl(),
-      taskName:new FormControl()
+      taskDescription: new FormControl(),
+      taskType: new FormControl(),
+      taskName: new FormControl()
     });
 
    }
 
-  ngOnInit() {
-    this.selectionList.selectionChange.subscribe((s: MatSelectionListChange) => {   
-        this.selectionList.deselectAll();
-        if(this.selected!=s.option){
-          this.selected=s.option
-          s.option.selected = true;
-          //fill the form by using the current task
-        }else{
-          this.selected=null;
-          s.option.selected=false;
-          //clear all the values from the form
-          this.taskFormGroup.reset()
-        }
-  });
-
-  }
+  ngOnInit() { }
 
 }
 
